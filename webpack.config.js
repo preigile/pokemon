5518fb1307e102e9a386c7ef054b4e59687327fc
chichikova.i <chichikova.i@medindex.ru>
2021-05-16 14:46:43 +0300

    set fonts

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,18 @@ module.exports = {
         test: /\.svg$/,
         use: ['@svgr/webpack', 'url-loader'],
       },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
