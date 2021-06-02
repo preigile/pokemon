183d00cd65980bd901b2b8fb0daad0b9082478a8
chichikova.i <chichikova.i@medindex.ru>
2021-06-02 21:24:10 +0300

    Render React Component

--- server/server.js
+++ server/server.js
@@ -1,31 +1,31 @@
-'use strict';
-
-const Hapi = require('@hapi/hapi');
+import Hapi from '@hapi/hapi';
+import React from 'react';
+import ReactDom from 'react-dom/server';
+import { setPath } from 'hookrouter';
+import App from '../src/App';
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 3000,
-        host: 'localhost'
-    });
+  server.route({
+    method: 'GET',
+    path: '/{any*}',
+    handler: (request, h) => {
+      setPath(request.path);
+      return ReactDom.renderToString(<App />);
+    },
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/{any*}',
-        handler: (request, h) => {
-
-            return `Hello World! page ${request.path}`;
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
